dd8bcd9907f2af40e881e1f32c5f2fdf274bc455
Ted Meyer <tmathmeyer@gmail.com>
2014-09-03 23:16:18 -0400

    updated to starter file

--- hw2/homework2.rkt
+++ hw2/homework2.rkt
@@ -1,46 +1,87 @@
 #lang plai-typed
 
+;; starter file for the extended basic interpreter
 
-(define-type expr
-  (plusE (l : expr) (r : expr))
-  (multE (l : expr) (r : expr))
-  (subtE (l : expr) (r : expr))
-  (id (id : symbol))
-  (lambda (fp : (listof expr)) (b : expr)) ;fp = formal parameters
-  (if0 (e : expr) (t : expr) (f : expr)))
-  
-(define-type nvar
-  (var (lbl : symbol) (eval : expr)))
+;; surface syntax and parser 
 
+; type used to capture a with-binding
+(define-type DefS
+  [defS (name : symbol) (val : ExprS)])
 
+(define-type ExprS
+  [numS (n : number)]
+  [plusS (l : ExprS) (r : ExprS)]
+  [bminusS (l : ExprS) (r : ExprS)]
+  [multS (l : ExprS) (r : ExprS)]
+  [idS (i : symbol)]
+  [appS (f : ExprS) (arg : ExprS)]
+  [if0S (c : ExprS) (t : ExprS) (e : ExprS)]
+  [lamS (arg : symbol) (body : ExprS)]
+  [withS (bindings : (listof DefS)) (body : ExprS)]
+  )
 
-(define e
+; parses s-expressions into surface syntax
+(define (parse [s : s-expression]) : ExprS
+  (cond
+    [(s-exp-number? s) (numS (s-exp->number s))]
+    [(s-exp-symbol? s) (idS (s-exp->symbol s))]
+    [(s-exp-list? s)
+     (let ([sl (s-exp->list s)])
+       (cond [(s-exp-symbol? (first sl)) ;; built-in construct or calling function through an identifier
+              (case (s-exp->symbol (first sl))
+                [(+) (plusS (parse (second sl)) (parse (third sl)))]
+                [(*) (multS (parse (second sl)) (parse (third sl)))]
+                [(-) (bminusS (parse (second sl)) (parse (third sl)))]
+                [(if0) (if0S (parse (second sl)) (parse (third sl)) (parse (fourth sl)))]
+                [(lambda) (lamS (s-exp->symbol (second sl)) (parse (third sl)))]
+                [(with) (withS (map (lambda (b) 
+                                      (let ([bl (s-exp->list b)])
+                                        (defS (s-exp->symbol (first bl)) (parse (second bl)))))
+                                    (s-exp->list (second sl)))
+                               (parse (third sl)))]
+                [else ;; must be a function application
+                 (appS (idS (s-exp->symbol (first sl)))
+                       (parse (second sl)))])]
+             [(s-exp-list? (first sl)) ;; app with complex expression in function position
+              (appS (parse (first sl))
+                    (parse (second sl)))]
+             [else (error 'parse "expected symbol or list after parenthesis")]))]
+    [else (error 'parse "unexpected input format")]))
+     
+;; abstract syntax and desugar
+     
+(define-type ExprC
+  [numC (n : number)]
+  [plusC (l : ExprC) (r : ExprC)]
+  [multC (l : ExprC) (r : ExprC)]
+  [idC (i : symbol)]
+  [appC (f : ExprC) (arg : ExprC)]
+  [if0C (c : ExprC) (t : ExprC) (e : ExprC)]
+  [lamC (arg : symbol) (body : ExprC)]
+  )
 
+;; output values
 
+(define-type Value
+  [numV (n : number)]
+  [closV (arg : symbol) (body : ExprC) (env : Env)])
 
+;; Environments
 
+;; binding an identifier to a value
+(define-type Binding
+  [bind (name : symbol) (val : Value)])
+ 
+(define-type-alias Env (listof Binding))
+(define mt-env empty)
 
+;; API for running programs
 
+; evaluates a program starting with a pre-populated environment
+; (this can be helpful in testing)
+(define (run/env sexp env)
+  (interp (desugar (parse sexp)) env))
 
-
-;; expr -> number
-(define (interp (src : expr)) : number
-  (interp-with-context src empty))
-
-;; expr -> [nvar] -> number
-(define (interp-with-context (src : expr) (context : (listof nvar))) : number
-  (type-case expr src
-    (plusE (l r) (+ (interp-with-context l context)
-                    (interp-with-context r context)))
-    (multE (l r) (* (interp-with-context l context)
-                    (interp-with-context r context)))
-    (subtE (l r) (- (interp-with-context l context)
-                    (interp-with-context r context)))
-    (id    (i)   (interp-with-context (lookup-id context i) context))
-    (else 0)))
-
-;; [nvar] -> symbol -> expr
-(define (lookup-id (context : (listof nvar)) (find : symbol)) : expr
-  (let ((results (filter (λ (v) (symbol=? (var-lbl v) find)) context)))
-    (cond ((cons? results) (var-eval (first results)))
-          (else (error 'failed "failed")))))
+; evaluates a program in the empty environment
+(define (run sexp)
+  (run/env sexp mt-env))
